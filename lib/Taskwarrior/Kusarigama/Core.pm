54dc8173bbab733d7475677a98c32ae6600f7a90
Yanick Champoux <yanick@cpan.org>
2017-02-20 15:30:03 -0500

    documentation

--- lib/Taskwarrior/Kusarigama/Core.pm
+++ lib/Taskwarrior/Kusarigama/Core.pm
@@ -1,4 +1,15 @@
 package Taskwarrior::Kusarigama::Core;
+# ABSTRACT: Set of core functions interacting with Taskwarrior
+
+=head1 DESCRIPTION
+
+Role consumed by L<Taskwarrior::Kusarigama::Hook>. 
+
+=head1 METHODS
+
+The role provides the following methods:
+
+=cut
 
 use strict;
 use warnings;
@@ -18,20 +29,47 @@ use experimental 'postderef';
 
 use namespace::clean;
 
+=head2 api
+
+=head2 version
+
+=head2 args
+
+=head2 command
+
+=head2 rc
+
+=head2 data
+
+=cut
+
 has $_ => (
     is => 'rw',
 ) for  qw/ api version args command rc data /;
 
+=head2 data_dir
 
+=cut
 
 has data_dir => sub {
     path( $_[0]->data );
 };
 
+=head2 feedback
+
+=cut
+
 has feedback => (
     is => 'rw',
     default => sub { [] },
 );
+
+=head2 plugins
+
+Returns an arrayref of instances of the plugins defined 
+under Taskwarrior's C<kusarigama.plugins> configuration key.
+
+=cut
 
 has plugins => sub {
     my $self = shift;
@@ -40,20 +78,40 @@ has plugins => sub {
 
     [ map { use_module($_)->new( tw => $self ) }
     map { s/^\+// ? $_ : ( 'Taskwarrior::Kusarigama::Plugin::' . $_ ) }
-            split ',', $self->config->{twhooks}{plugins} ]
+            split ',', $self->config->{kusarigama}{plugins} ]
 };
 
 before plugins => sub {
     my $self = shift;
     @INC = uniq @INC,  
         map { s/^\./$self->data_dir/er }
-        split ':', $self->config->{twhooks}{lib};
+        split ':', $self->config->{kusarigama}{lib};
 };
+
+=head2 add_feedback
+
+    $tw->add_feeback( $message );
+
+Add an output line to be returned to the end-user.
+
+=cut
 
 sub add_feedback {
     my $self = shift;
     push $self->feedback->@*, @_;
 }
+
+=head2 export_tasks
+
+    my @tasks = $tw->export_tasks( @query );
+
+Equivalent to
+
+    $ task export ...query...
+
+Returns the list of the tasks.
+
+=cut
 
 sub export_tasks {
     my( $self, @query ) = @_;
@@ -63,6 +121,16 @@ sub export_tasks {
     return @{ from_json $out };
 }
 
+=head2 import_task
+
+    $tw->import_task( \%task  )
+
+Equivalent to
+
+    $ task import <json representation of %task>
+
+=cut
+
 sub import_task {
     my( $self, $task ) = @_;
 
@@ -70,6 +138,16 @@ sub import_task {
 
     run3 [qw/ task rc.recurrence=no import /], \$in;
 }
+
+=head2 calc
+
+    $result = $tw->calc( qw/ today + 3d / );
+
+Equivalent to
+
+    $ task calc today + 3d
+
+=cut
 
 sub calc {
     my( $self, @stuff ) = @_;

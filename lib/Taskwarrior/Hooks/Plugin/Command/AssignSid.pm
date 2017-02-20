248a8a4ac1abee2575730e7f6aee2cd3897ab9a1
Yanick Champoux <yanick@cpan.org>
2017-02-20 11:27:15 -0500

    AssignSid plugin

---
+++ lib/Taskwarrior/Hooks/Plugin/Command/AssignSid.pm
@@ -0,0 +1,24 @@
+package Taskwarrior::Hooks::Plugin::Command::AssignSid;
+
+use strict;
+use warnings;
+
+use Moo;
+
+extends 'Taskwarrior::Hooks::Hook';
+
+with 'Taskwarrior::Hooks::Hook::OnCommand';
+
+sub on_command {
+    my $self = shift;
+
+    my $args = $self->args;
+    $args =~ s/hide\s*(.*)/ 'mod wait:' . ($1 || '1day')/e;
+
+    system $args;
+};
+
+
+1;
+
+__END__

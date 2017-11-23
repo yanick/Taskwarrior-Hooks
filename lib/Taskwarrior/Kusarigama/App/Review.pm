0c0ff0b98d911d015a11ca359513c2163d02fad4
Yanick Champoux <yanick@cpan.org>
2017-11-23 15:47:21 -0500

    more work on the review command

--- lib/Taskwarrior/Kusarigama/App/Review.pm
+++ lib/Taskwarrior/Kusarigama/App/Review.pm
@@ -23,6 +23,9 @@ use Term::ANSIColor qw/ colored /;
 use MooseX::App::Command;
 use MooseX::MungeHas;
 
+parameter subcommand => (
+    is => 'ro',
+);
 
 use experimental 'postderef', 'signatures';
 
@@ -31,27 +34,116 @@ has tw => sub {
 };
 
 has tasks => sub {
-    return +{ partition_by { $_->{priority} || 'U' } $_[0]->tw->export( '+PENDING' ) };
+    return +{ partition_by { $_->{priority} || 'U' } $_[0]->tw->export( '+READY' ) };
 };
+
+sub async_do ($self,$code) {
+    return if fork;
+
+    $code->();
+    exit;
+}
+
+use IO::Prompt::Simple;
+use Prompt::ReadKey ();
+
+
+has _prompt => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { Prompt::ReadKey->new; },
+    handles => { 'menu_prompt' => 'prompt' },
+);
+
+sub nbr_prioritized_tasks($self) {
+    use List::AllUtils qw/ sum /;
+    return sum map { scalar $self->tasks->{$_}->@* } qw/ H M L /;
+}
+
+sub decimate($self) {
+    $self->decimate_group( 0.10, 'H', 'M' );
+    $self->clear_tasks;
+    $self->decimate_group( 0.60, 'M', 'L' );
+}
+
+sub decimate_group($self,$target,$prio1,$prio2) {
+    my $nbr = int $target * $self->nbr_prioritized_tasks;
+
+    my $main = $self->tasks->{$prio1};
+
+    say "Decimating $prio1 priority...";
+    say "We have @{[ scalar @$main ]}, we want $nbr";
+
+    # do we have enough?
+    while( $nbr > @$main ) {
+        say "need ", $nbr - @$main, ' more ', $prio1, ' tasks';
+        say "Promote one!\n\n";
+        my $task = $self->pick_decimate( $self->tasks->{$prio2} );
+
+        push @$main, $task;
+        $self->async_do(sub{
+                $task->mod( 'priority:' . $prio1 );
+        });
+    }
+
+    # do we have too much?
+    while( $nbr < @$main ) {
+        say "need ", @$main - $nbr, ' less ', $prio1, ' tasks';
+        say "Demote one!\n\n";
+        my $task = $self->pick_decimate( $self->tasks->{$prio1} );
+
+        shift @$main;
+        $self->async_do(sub{
+                $task->mod( 'priority:' . $prio2);
+        });
+    }
+}
+
+sub pick_decimate($self, $tasks ) {
+    use List::AllUtils qw/ shuffle /;
+    my @contenders = (shuffle @$tasks)[0..9];
+
+    use DDP;
+    use Term::ANSIColor;
+
+    for ( 0..9 ) {
+        my $c = $contenders[$_];
+
+        printf "%2d %4d %s%s%s\n", 
+            $_, $contenders[$_]{id}, 
+            colored( ['blue'], $c->{project} ? '['.$c->{project}.'] ' : '' ),
+            $contenders[$_]{description},
+            ( join ' ', map { colored [ 'cyan'  ], " +$_" } @{ $c->{tags} } );
+    }
+
+    print "\n\n";
+
+    my $action = $self->menu_prompt( prompt => "which one?",
+        options => [
+            map { +{  keys => [ $_ ], name => $_ } } 0..9
+        ],
+        help_keys => [ '?' ],
+    );
+
+    @$tasks = grep { $_->{uuid} ne $contenders[$action]->{uuid} } @$tasks;
+
+    return $contenders[$action];
+}
 
 sub run {
     my $self = shift;
+
+    return $self->decimate if $self->subcommand eq 'decimate';
 
     while ( my $next = eval { shift $self->tasks->{U}->@* } ) {
         use Term::ANSIScreen qw/:screen :cursor/;
         cls;
         $self->print_summary_line;
 
-
-        use IO::Prompt::Simple;
-        use Prompt::ReadKey ();
-
-        my $p = Prompt::ReadKey->new;
-
         while() {
             say join "\n", $self->tw->info( $next->{uuid} );
 
-            my $action = $p->prompt( prompt => "whatcha gonna do?",
+            my $action = $self->menu_prompt( prompt => "whatcha gonna do?",
                 case_insensitive => 0,
                 options => [
                     { name => 'h', doc => 'high priority', keys => [ 'h' ] },
@@ -63,25 +155,47 @@ sub run {
                     { name => 'mod', doc => 'generic modification', keys => [ '.' ] },
                     { name => 'done', keys => [ 'd' ] },
                     { name => 'delete', keys => [ 'D' ] },
+                    { name => 'wait', keys => [ 'w' ] },
+                    { name => 'quit', keys => [ 'q' ] },
+                    { name => 'annotate', keys => [ 'a' ] },
                     { name => 'next', doc => 'next', keys => [ 'n' ] },
                 ],
                 help_keys => [ '?' ],
             );
 
+            if ( $action eq 'quit' ) {
+                say "no, come back!";
+                return;
+            }
+
+            if ( $action eq 'annotate' ) {
+                $next->annotate( prompt 'note' );
+            }
+            elsif ( $action eq 'wait' ) {
+                $self->wait_menu($next);
+                last;
+            }
             if( $action =~ /^[hml]$/i ) {
-                $next->mod( 'priority:' . uc $action );
+                if ( $action  eq uc $action ) {
+                    $self->async_do(sub{
+                        $next->mod( 'priority:' . uc $action );
+                    });
+                }
+                else {
+                    $next->mod( 'priority:' . uc $action );
+                }
             }
             elsif ( $action eq 'mod' ) {
                 $next->mod( prompt "mod" );
             }
             elsif ( $action eq 'done' ) {
-                $next->done;
+                $self->async_do(sub{ $next->done; });
                 last;
             }
             elsif ( $action eq 'delete' ) {
                 # TODO I think it fails because underneath it tries to ask
                 # interactively if deleting is okay
-                $next->delete;
+                $next->delete( { 'rc.confirmation' => 'no' } );
                 last;
             }
 
@@ -94,6 +208,29 @@ sub run {
 
     say "congrats! no task left unprioritized!";
 
+}
+
+sub wait_menu($self,$task) {
+
+    my $action = $self->menu_prompt( prompt => "how long?",
+        case_insensitive => 0,
+        options => [
+            { name => 'eow', doc => 'end of week priority', keys => [ 'e' ] },
+            { name => '1w', doc => 'one week', keys => [ 'w' ] },
+            { name => '1m', doc => 'one month', keys => [ 'm' ] },
+            { name => '3m', doc => 'three months', keys => [ 'M' ] },
+            { name => 'edit', doc => 'custom', keys => [ '.' ] },
+        ],
+        help_keys => [ '?' ],
+    );
+
+    if( $action eq 'edit' ) {
+        $action = prompt 'wait';
+    }
+
+    $self->async_do(sub{ 
+        $task->mod( 'wait:'.$action );
+    });
 }
 
 sub print_summary_line($self) {

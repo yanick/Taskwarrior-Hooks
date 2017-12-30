802e9253b36a57ed79378e2aa1c0a125763a8aee
Yanick Champoux <yanick@cpan.org>
2017-12-30 11:31:22 -0500

    have 'task-kusarigama add Foo'  run the plugin setups

--- lib/Taskwarrior/Kusarigama/App/Add.pm
+++ lib/Taskwarrior/Kusarigama/App/Add.pm
@@ -1,12 +1,20 @@
 package Taskwarrior::Kusarigama::App::Add;
 # ABSTRACT: Add plugins to Taskwarrior
 
+=head1 SYNOPSIS
+
+    $ task-kusarigama add Command::Open Renew
+
+=cut
+
 use 5.10.0;
 
 use strict;
 use warnings;
 
 use List::AllUtils qw/ uniq /;
+use Set::Object qw/ set /;
+use Module::Runtime qw/ use_module /;
 
 use Taskwarrior::Kusarigama;
 
@@ -20,11 +28,21 @@ extends 'Taskwarrior::Kusarigama::App';
 sub run {
     my $self = shift;
 
-    my @plugins = uniq( ( map { $_->name } $self->tw->plugins->@* ), $self->extra_argv->@* );
+    my $old_plugins = set( map { $_->name } $self->tw->plugins->@* );
 
-    say "setting plugins to ", join ', ', @plugins;
+    my $new_plugins = set($self->extra_argv->@*) - $old_plugins;
 
-    system 'task', 'config', 'kusarigama.plugins', join ',', @plugins;
+    my $plugins = $old_plugins + $new_plugins;
+
+    say "setting plugins to ", join ', ', @$plugins;
+
+    $self->tw->run_task->config( [{ 'rc.confirmation' => 'off' }], 'kusarigama.plugins', join ',', @$plugins );
+
+    $_->new( tw => $self->tw )->setup for
+        grep { use_module($_)->can('setup') } 
+        map { "Taskwarrior::Kusarigama::Plugin::$_" }
+            @$new_plugins;
+
 }
 
 1;
